d2d0a999c5d42b93a428da9fa310e1a41810ec3f
Wassup Chicken <74932143+wassup-chicken@users.noreply.github.com>
2024-11-04 10:43:17 -0500

    test

--- dog.go
+++ dog.go
@@ -1,9 +1,11 @@
 package dog
 
 import (
+	"fmt"
 	"strings"
 )
 
 func WhenGrownUp(s string) string {
+	fmt.Println("hello world")
 	return "When the puppy grows up it says: " + strings.ToUpper(s)
 }
